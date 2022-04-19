a669b24bb2fc5f60eb2907ea9128e409850115af
paulodavid5 <paulodaviddias4@gmail.com>
2022-04-19 17:47:03 +0100

    js buttontop

--- app.js
+++ app.js
@@ -292,3 +292,18 @@ toggle.addEventListener('click', () => {
 
 })
 
+mybutton = document.getElementById("myBtn")
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+function topFunction() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
